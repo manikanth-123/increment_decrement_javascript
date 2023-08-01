b55960a940598b8b044e2a67313a45da4521f5f9
manikanth-123 <64304070+manikanth-123@users.noreply.github.com>
2023-08-01 10:47:22 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template:"<h1 style='text-align:center'>Tutorialswebsite.com</h1>",
+  styleUrls: ['./app.component.scss']
+  //styles:['h1{color:blue}']
+})
+export class AppComponent {
+
+  users = {
+    name: "",
+    email: "",
+    mobile: ''
+  }
+
+  contactForm = new FormGroup({
+
+    yourname: new FormControl(''),
+    email: new FormControl(''),
+    mobile: new FormControl(''),
+
+  });
+
+  formSubmit(val: any): void {
+
+    // console.log(val);
+    var name = val.yourname;
+    var email = val.email;
+    var mobile = val.mobile;
+    console.log(name + ", " + email + ", " + mobile);
+  }
+
+}
